0f3dc69063e116a70b2495954a359c9c533c6d79
Imran0316 <ik775239@gmail.com>
2024-10-21 07:31:37 -0700

    add all files

---
+++ basic.php
@@ -0,0 +1,92 @@
+<?php
+
+// $a = "imrna";
+// $b = 145;
+// $C = 43.34;
+
+// var_dump(  $a);
+// var_dump(  $b);
+// var_dump(  $C);
+
+// $x = 10;
+// if($x>=10){
+//     echo "X is greater than 10 " . "<br>";
+// }else{
+//     echo "x is smaller then 10";
+// }
+
+// $num = 24;
+// if($num%2==0){
+//     echo "this is an even number";
+// }else{
+//     echo "this is odd number";
+// }
+
+// $marks = 100;
+// if($marks>=40 && $marks<= 100){
+//     echo"congragulation👏"."<br>" ;
+//     echo " you  are elgible for admission";
+// }else{
+//     echo "you are not eligible";
+// }
+
+// $marks = 40;
+// if($marks>=40 xor $marks<=100){
+//     echo"you are eligible";
+// }else{
+//     echo "you are not";
+// }
+
+// $name = "subhna";
+// $gender = "male";
+// if($gender == "male"){
+//     echo " Hello MR, {$name} hope you are well.";
+// }else{
+//     echo "hello mrs".$name;
+// }
+
+
+// $per = 90;
+// if($per>=80 && $per<= 100){
+//     echo"you got A+ grade🎉 . your percentage is {$per}";
+// }else if($per>=70 && $per<80){
+//     echo "you got A grade👌. your percentage is {$per}";
+// }else if($per>=60 && $per<70){
+//     echo "you got B grade👍. your percentage is {$per}";
+// }else if($per>=50 && $per<60){
+//     echo "you got C grade👍. your percentage is {$per}";
+// }else if($per>=33 && $per<50){
+//     echo "you got D grade👍. your percentage is {$per}";
+// }else{
+//     echo "you are fail 😢. your percentage is {$per}";
+// }
+
+// $weekday = 3;
+// switch($weekday){
+//     case 1:
+//         echo "today is monday";
+//     break;  
+//     case 2:
+//         echo "today is tuesday";
+//     break;   
+//     case 3:
+//         echo "today is wednesday";
+//     break;   
+//     case 4:
+//         echo "today is thursday";
+//     break;   
+//     case 5:
+//         echo "today is friday";
+//     break;
+//     case 6:
+//         echo "today is saturday";
+//     break; 
+//     case 7:
+//         echo "today is sunday";
+//     break;  
+//     default;
+//     echo"invalid input";    
+// }
+
+
+?>
